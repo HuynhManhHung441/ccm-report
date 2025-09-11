1c138730bbf5d08014b1e7e67642337e13714ab2
HuynhManhHung441 <huynhmanhhung441@gmail.com>
2025-09-11 21:47:53 +0700

    add logic for Search form

--- src/App.js
+++ src/App.js
@@ -20,42 +20,61 @@ import SlabData from './components/SlabData';
 import SlabSectionData from './components/SlabSectionData';
 
 function App() {
-  const heatName = '25F003353';
+  const [heatName, setHeatName] = useState('25F003353');
+  const [heatNameInput, setHeatNameInput] = useState();
+
+  const handleSearch = () => {
+    if (!heatNameInput.trim()) return;
+    setHeatName(heatNameInput.trim());
+    setHeatNameInput("");
+  };
+
   return (
     <div className="container">
-      <HeatReportHeader heatName={heatName} />
-      <div className="section-row">
-        {/* Cột 1: General + Operator Comment */}
-        <div className="column">
-          <GeneralSection heatName={heatName} />
-          <OperatorCommentSection />
-          <TundishTempSporadic heatName={heatName} />
-        </div>
+      <div id="heat-report__content">
+        <HeatReportHeader heatName={heatName} />
+        <div className="section-row">
+          {/* Cột 1: General + Operator Comment */}
+          <div className="column">
+            <GeneralSection heatName={heatName} />
+            <OperatorCommentSection />
+            <TundishTempSporadic heatName={heatName} />
+          </div>
 
-        {/* Cột 2: Ladle + Shroud + Steel Loss */}
-        <div className="column">
-          <LadleSection heatName={heatName} />
-          <ShroudSection heatName={heatName} />
-          <SteelLossSection heatName={heatName} />
-          <TundishTempContinuous />
-        </div>
+          {/* Cột 2: Ladle + Shroud + Steel Loss */}
+          <div className="column">
+            <LadleSection heatName={heatName} />
+            <ShroudSection heatName={heatName} />
+            <SteelLossSection heatName={heatName} />
+            <TundishTempContinuous />
+          </div>
 
-        {/* Cột 3: Ladle Arrival */}
-        <div className="column">
-          <LadleArrivalSection heatName={heatName} />
-          <LadleDepartureSection heatName={heatName} />
-        </div>
+          {/* Cột 3: Ladle Arrival */}
+          <div className="column">
+            <LadleArrivalSection heatName={heatName} />
+            <LadleDepartureSection heatName={heatName} />
+          </div>
 
-        {/* Cột 4: Tundish */}
-        <div className="column">
-          <TundishSection heatName={heatName} />
-          <TundishMaterialSection heatName={heatName} />
+          {/* Cột 4: Tundish */}
+          <div className="column">
+            <TundishSection heatName={heatName} />
+            <TundishMaterialSection heatName={heatName} />
+          </div>
         </div>
+        <StrandDataSection heatName={heatName} />
+        <AnalysisDataSection heatName={heatName} />
+        <SlabData heatName={heatName} />
+        <SlabSectionData heatName={heatName} />
       </div>
-      <StrandDataSection heatName={heatName} />
-      <AnalysisDataSection heatName={heatName} />
-      <SlabData heatName={heatName} />
-      <SlabSectionData heatName={heatName} />
+      <div className="heat-report__search">
+        <input
+            type="text"
+            placeholder="🔍Heat Name"
+            value={heatNameInput}
+            onChange={(e)=>setHeatNameInput(e.target.value)}
+          />
+        <button onClick={handleSearch}>Search</button>
+      </div>
     </div>
   );
 }
